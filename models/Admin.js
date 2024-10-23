9d702baccc0571cb73c54802e4549dd87e620090
baseline090 <100409036+baseline090@users.noreply.github.com>
2024-10-23 15:51:53 +0530

    Add files via upload

---
+++ models/Admin.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const adminSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6 // Set minimum length for password
+  }
+});
+
+// Hash password before saving
+adminSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+module.exports = mongoose.model('Admin', adminSchema);
+
+
