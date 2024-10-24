c270990b3cd16aef3e484f52db9728a52a54831d
baseline090 <100409036+baseline090@users.noreply.github.com>
2024-10-24 16:02:01 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,3 @@
-
-// const jwt = require('jsonwebtoken');
-
-// const auth = (req, res, next) => {
-//   const token = req.headers['authorization']?.split(' ')[1]; // Get the token from the header
-
-//   if (!token) {
-//     return res.status(401).json({ message: 'No token provided' });
-//   }
-
-//   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-//     if (err) {
-//       return res.status(401).json({ message: 'Unauthorized' });
-//     }
-//     req.user = decoded; // Attach user data to the request
-//     next(); // Proceed to the next middleware or route handler
-//   });
-// };
-
-// module.exports = auth;
-
-
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
