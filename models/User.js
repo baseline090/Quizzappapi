6ecedc1771483b6804a38136ce26b72629011a3a
baseline090 <100409036+baseline090@users.noreply.github.com>
2024-10-24 16:00:57 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
+    fullName: { type: String, required: true },
     firstName: { type: String, default: "" }, // Optional during registration
     lastName: { type: String, default: "" },  // Optional during registration
     username: { type: String, required: true },
