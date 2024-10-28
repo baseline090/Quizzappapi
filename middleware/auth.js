a4f03c0140d9017f3896878df19088b20055c5c5
baseline090 <100409036+baseline090@users.noreply.github.com>
2024-10-28 08:41:25 +0530

    Update auth.js
    Message updation

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,39 @@
+// const jwt = require('jsonwebtoken');
+
+// const auth = (req, res, next) => {
+//   const token = req.header('Authorization').replace('Bearer ', '');
+//   if (!token) {
+//     return res.status(401).json({ message: 'Unauthorized' });
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decoded; // Attach the user data to the request object
+//     next();
+//   } catch (error) {
+//     return res.status(401).json({ message: 'Unauthorized' });
+//   }
+// };
+
+// module.exports = auth;
+
+
+
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
-  const token = req.header('Authorization').replace('Bearer ', '');
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+
   if (!token) {
-    return res.status(401).json({ message: 'Unauthorized' });
+    return res.status(401).json({ message: 'Unauthorized access, please login first' });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Attach the user data to the request object
+    req.user = decoded;
     next();
   } catch (error) {
-    return res.status(401).json({ message: 'Unauthorized' });
+    return res.status(401).json({ message: 'Unauthorized access, please login first' });
   }
 };
 
