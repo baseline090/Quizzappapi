d175acc02e7bb401fe39ba02640924021f4d458c
baseline090 <100409036+baseline090@users.noreply.github.com>
2024-11-04 14:39:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -89,11 +89,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-
-
-
-// // Server listening on PORT
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`Server running on http://localhost:${PORT}`);
-// });
