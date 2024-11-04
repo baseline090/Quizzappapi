4f52a3bc11988278656fcdf276ee95247822b208
baseline090 <100409036+baseline090@users.noreply.github.com>
2024-11-04 14:48:20 +0530

    Update tokenBlacklist.js

--- utils/tokenBlacklist.js
+++ utils/tokenBlacklist.js
@@ -1,13 +1,4 @@
 
-// const tokenBlacklist = new Set();
-
-// module.exports = {
-//   addToken: (token) => tokenBlacklist.add(token),
-//   isTokenBlacklisted: (token) => tokenBlacklist.has(token),
-// };
-
-
-
 const tokenBlacklist = new Set();
 
 module.exports = {
