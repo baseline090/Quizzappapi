5ad96f322b4d6c4e9aa1e6a19413025c7a2bd003
baseline090 <100409036+baseline090@users.noreply.github.com>
2024-11-06 15:57:41 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,30 +1,3 @@
-// const jwt = require('jsonwebtoken');
-// const tokenBlacklist = require('../utils/tokenBlacklist');
-
-// const auth = (req, res, next) => {
-//   const token = req.header('Authorization')?.replace('Bearer ', '');
-
-//   if (!token) {
-//     return res.status(401).json({ message: 'Unauthorized access, please login first' });
-//   }
-
-//   // Check if the token is blacklisted
-//   if (tokenBlacklist.isTokenBlacklisted(token)) {
-//     return res.status(401).json({ message: 'Token is invalid. Please login again.' });
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     req.user = decoded; // Attach user info to request
-//     next(); // Continue to the next middleware or route handler
-//   } catch (error) {
-//     return res.status(401).json({ message: 'Unauthorized access, please login first' });
-//   }
-// };
-
-// module.exports = auth;
-
-
 
 
 const jwt = require('jsonwebtoken');
