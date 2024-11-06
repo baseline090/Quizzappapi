06a6d3b44b99045df148d3f7f867e1c1d509873d
baseline090 <100409036+baseline090@users.noreply.github.com>
2024-11-06 15:58:06 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,30 +1,3 @@
-// const mongoose = require('mongoose');
-// const bcrypt = require('bcrypt');
-
-// const userSchema = new mongoose.Schema({
-//     fullName: { type: String, required: true },
-//     firstName: { type: String, default: "" }, // Optional during registration
-//     lastName: { type: String, default: "" },  // Optional during registration
-//     username: { type: String, required: true },
-//     email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ }, // Regex for email validation
-//     password: { type: String, required: true },
-//     otp: { type: String, default: null }, // Field for storing OTP
-//     phoneNumber: { type: String, default: null }, // Optional during registration
-//     // profilePic: { type: Buffer }// Optional during registration
-// }, { timestamps: true });
-
-
-// // Hash password before saving
-// userSchema.pre('save', async function (next) {
-//     if (this.isModified('password')) {
-//         this.password = await bcrypt.hash(this.password, 10);
-//     }
-//     next();
-// });
-
-// module.exports = mongoose.model('User', userSchema);
-
-
 
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
