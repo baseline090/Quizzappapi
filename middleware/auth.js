8a0a058b67b235e06970074beaaebb7de879d720
baseline090 <100409036+baseline090@users.noreply.github.com>
2024-11-06 14:06:55 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,38 @@
+// const jwt = require('jsonwebtoken');
+// const tokenBlacklist = require('../utils/tokenBlacklist');
+
+// const auth = (req, res, next) => {
+//   const token = req.header('Authorization')?.replace('Bearer ', '');
+
+//   if (!token) {
+//     return res.status(401).json({ message: 'Unauthorized access, please login first' });
+//   }
+
+//   // Check if the token is blacklisted
+//   if (tokenBlacklist.isTokenBlacklisted(token)) {
+//     return res.status(401).json({ message: 'Token is invalid. Please login again.' });
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decoded; // Attach user info to request
+//     next(); // Continue to the next middleware or route handler
+//   } catch (error) {
+//     return res.status(401).json({ message: 'Unauthorized access, please login first' });
+//   }
+// };
+
+// module.exports = auth;
+
+
+
+
 const jwt = require('jsonwebtoken');
 const tokenBlacklist = require('../utils/tokenBlacklist');
 
 const auth = (req, res, next) => {
+  // console.log(JSON.stringify(req.headers));
+
   const token = req.header('Authorization')?.replace('Bearer ', '');
 
   if (!token) {
@@ -14,10 +45,12 @@ const auth = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // Verify the token and its expiration time
+    const decoded = jwt.verify(token, process.env.JWT_SECRET, { expiresIn: '24h' });
     req.user = decoded; // Attach user info to request
     next(); // Continue to the next middleware or route handler
   } catch (error) {
+    // Handle token expiration or other verification errors
     return res.status(401).json({ message: 'Unauthorized access, please login first' });
   }
 };
