16bfd651c4a274865c89fa438a33faa094776b5e
baseline090 <100409036+baseline090@users.noreply.github.com>
2024-11-06 13:52:11 +0530

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,3 +1,34 @@
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
+
+// const adminSchema = new mongoose.Schema({
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     trim: true,
+//     lowercase: true
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     minlength: 6 // Set minimum length for password
+//   }
+// });
+
+// // Hash password before saving
+// adminSchema.pre('save', async function (next) {
+//   if (this.isModified('password')) {
+//     this.password = await bcrypt.hash(this.password, 10);
+//   }
+//   next();
+// });
+
+// module.exports = mongoose.model('Admin', adminSchema);
+
+
+
+
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
@@ -14,15 +45,15 @@ const adminSchema = new mongoose.Schema({
     required: true,
     minlength: 6 // Set minimum length for password
   }
-});
+},{timestamps : true});
 
-// Hash password before saving
-adminSchema.pre('save', async function (next) {
-  if (this.isModified('password')) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-  next();
-});
+// // Hash password before saving
+// adminSchema.pre('save', async function (next) {
+//   if (this.isModified('password')) {
+//     this.password = await bcrypt.hash(this.password, 10);
+//   }
+//   next();
+// });
 
 module.exports = mongoose.model('Admin', adminSchema);
 
